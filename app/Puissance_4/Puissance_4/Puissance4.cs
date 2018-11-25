d9f51be0a30e13515374491d6e0f19d4a1f6ea73
unknown <marie.lahondere@hotmail.fr>
2018-11-25 12:41:19 +0100

    First commmmmmmmmmit

---
+++ app/Puissance_4/Puissance_4/Puissance4.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Puissance_4
+{
+    public class Puissance4 : Jeu
+    {
+        public Puissance4() : base("Jeu")
+        {
+
+        }
+    }
+}
