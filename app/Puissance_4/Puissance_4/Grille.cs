5eed55083b78cf0e8d6dae41cb41a6f9446734d7
Timothée Gris <timo@airmail.cc>
2018-12-09 23:52:33 +0100

    Finished Grille.cs class

--- app/Puissance_4/Puissance_4/Grille.cs
+++ app/Puissance_4/Puissance_4/Grille.cs
@@ -2,39 +2,172 @@
 
 namespace Puissance_4
 {
-    class Grille
+    public class Grille
     {
+
+        private int _nbLignes, _nbColonnes, _compteurJeton;
+        private char _caseVide, _caseJoueur1, _caseJoueur2;
+        private char[,] _grilleJeu;
+
+        public int NbLignes
+        {
+            get => _nbLignes;
+            set => _nbLignes = value;
+        }
+
+        public int NbColonnes
+        {
+            get => _nbColonnes;
+            set => _nbColonnes = value;
+        }
+
+        public int CompteurJeton
+        {
+            get => _compteurJeton;
+            set => _compteurJeton = value;
+        }
+  
+        public char CaseVide
+        {
+            get => _caseVide;
+            set => _caseVide = value;
+        }
+
+        public char CaseJoueur1
+        {
+            get => _caseJoueur1;
+            set => _caseJoueur1 = value;
+        }
+
+        public char CaseJoueur2
+        {
+            get => _caseJoueur2;
+            set => _caseJoueur2 = value;
+        }
+
+        public char[,] GrilleJeu
+        {
+            get => _grilleJeu;
+            set => _grilleJeu = value;
+        }
+
         public Grille()
         {
-            private const int nbLignes = 6, nbColonnes = 7; // On donne le nombre de lignes et colonnes, ici 6*7
-            private const char caseVide = ' ', caseJoueur1 = '1', caseJoueur2 = '2'; // Les cases vides seront indiquees par un 0, celles jouees par le joueur 1, un 1, etc.
+            _nbLignes = 6;
+            _nbColonnes = 7;
+            _compteurJeton = 0;
 
-            private char[,] grilleJeu; // On initialise le tableau a deux dimensions qu'est la grille de jeu
+            _caseVide = ' ';
+            _caseJoueur1 = '1';
+            _caseJoueur2 = '2';
 
-            private int compteurJeton;
-
+            _grilleJeu = new char[_nbLignes, _nbColonnes];
+            
         }
 
         public void Init()
         {
-            grilleJeu = new char[nbLignes, nbColonnes]; // La grille a maintenant des dimensions definies
-
-            for (int i = 0; i < nbLignes; i++)
-                for (int j = 0; j < nbColonnes; j++)
-                    grilleJeu[i, j] = caseVide; // On initialise chaque case de la grille avec un case vide.
+            for (int i = 0; i < _nbLignes; i++)
+                for (int j = 0; j < _nbColonnes; j++)
+                    _grilleJeu[i, j] = _caseVide; // On initialise chaque case de la grille avec un case vide.
         }
 
         public void Afficher() // Methode pour afficher la grille sur la console
         {
-            for (int i = 0; i < nbLignes; i++)
+            for (int i = 0; i < _nbLignes; i++)
             {
-                for (int j = 0; j < nbColonnes; j++)
+                for (int j = 0; j < _nbColonnes; j++)
                 {
-                    Console.Write(grilleJeu[i, j]);
+                    Console.Write(_grilleJeu[i, j]);
                     Console.Write('|');
                 }
                 Console.WriteLine("");
             }
         }
+
+        public bool GrillePleine()
+        {
+            return _compteurJeton >= _nbColonnes * _nbLignes;
+        }
+
+
+        public bool Positionner(char _joueur, int _colonne) // retourne true si le jeton peut etre joue dans cette colonne
+        {
+            _colonne--;
+
+            if (_grilleJeu[0, _colonne] != _caseVide) // La case n'est pas vide, elle ne peut donc pas etre jouee : retourne faux
+                return false;
+
+            for (int i = 0; i < _nbLignes; i++)
+            {
+                if ((i == _nbLignes - 1) || (_grilleJeu[i + 1, _colonne] != _caseVide)) // La boucle check toutes les cases de la colonne pour voir s'il reste de la place
+                {
+                    _grilleJeu[i, _colonne] = _joueur; // Puis applique le jeton du joueur a cette place
+                    break;
+                }
+            }
+
+            _compteurJeton++;
+            return true;
+        }
+
+        public bool TestGagner(char joueur) // Methode qui check l'apparition d'une suite de 4 jetons
+        {
+            // Check horizontal
+            for (int i = 0; i < _nbLignes; i++)
+                for (int j = 0; j < 4; j++)
+                    if (_grilleJeu[i, j] == joueur && _grilleJeu[i, j + 1] == joueur)
+                        if (_grilleJeu[i, j + 2] == joueur && _grilleJeu[i, j + 3] == joueur)
+                        return true;
+
+            // Check vertical
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < _nbColonnes; j++)
+                    if (_grilleJeu[i, j] == joueur && _grilleJeu[i + 1, j] == joueur)
+                        if (_grilleJeu[i + 2, j] == joueur && _grilleJeu[i + 3, j] == joueur)
+                            return true;
+
+            // Check diagonal
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < _nbColonnes; j++)
+                {
+                    if (_grilleJeu[i, j] == joueur)
+                    {
+                        // Diagonale gauche
+                        try
+                        {
+                            if (_grilleJeu[i + 1, j - 1] == joueur)
+                            {
+                                if (_grilleJeu[i + 2, j - 2] == joueur)
+                                {
+                                    if (_grilleJeu[i + 3, j - 3] == joueur)
+                                        return true;
+                                }
+
+                            }
+                        }
+                        catch (IndexOutOfRangeException) {}
+
+                        // Diagonale droite
+                        try
+                        {
+                            if (_grilleJeu[i + 1, j + 1] == joueur)
+                            {
+                                if (_grilleJeu[i + 2, j + 2] == joueur)
+                                {
+                                    if (_grilleJeu[i + 3, j + 3] == joueur)
+                                        return true;
+                                }
+                            }
+                        }
+                        catch (IndexOutOfRangeException) {}
+                    }
+                }
+            }
+
+            return false; // Si aucune des boucles ne retournent true
+        }
+
     }
 }
